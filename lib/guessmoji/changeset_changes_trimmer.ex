d686fdbd8680daef0f531079dc5d4ccbe3ba5500
Elomar Nascimento dos Santos <elomarns@elomarns.com>
2018-03-06 12:22:23 -0300

    Trimming. Again. But a little better now.

--- lib/guessmoji/changeset_changes_trimmer.ex
+++ lib/guessmoji/changeset_changes_trimmer.ex
@@ -2,10 +2,11 @@ defmodule Guessmoji.ChangesetChangesTrimmer do
   import Ecto.Changeset
 
   def trim(%Ecto.Changeset{} = changeset, field) do
-    field_value =
-      get_change(changeset, field)
-      |> to_string()
-
-    put_change(changeset, field, String.trim(field_value))
+    if field_new_value = get_change(changeset, field) do
+      field_new_value = to_string(field_new_value)
+      put_change(changeset, field, String.trim(field_new_value))
+    else
+      changeset
+    end
   end
 end
