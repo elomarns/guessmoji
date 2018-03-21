e6b0f1f9d6e5625810c75a0361b9c9e33d72b3ff
Elomar Nascimento dos Santos <elomarns@elomarns.com>
2018-03-21 17:22:19 -0300

    Only emojis are allowed on this input.

--- assets/js/emoji_form.js
+++ assets/js/emoji_form.js
@@ -6,4 +6,17 @@ export function activateEmojiPicker() {
       shortcuts: false
     });
   });
+
+  $(document).on('focusout', '.emojionearea-editor', function() {
+    $(this).contents().filter(function() {
+      return this.nodeType === 3
+    }).remove()
+  })
+
+  $(document).on('change', '#emoji_content', function() {
+    const content = $(this).val()
+    const newContent = content.replace(/\w+/gi, '')
+
+    $(this).val(newContent)
+  })
 }
