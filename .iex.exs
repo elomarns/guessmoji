4faac745094064fe08b146ec83bb9831e1240aa1
Elomar Nascimento dos Santos <elomarns@elomarns.com>
2018-03-27 14:01:12 -0300

    My life is so much easy now.

---
+++ .iex.exs
@@ -0,0 +1,9 @@
+import Ecto.Query
+import Ecto.Changeset
+
+alias Guessmoji.Repo
+alias Guessmoji.Media
+alias Guessmoji.Media.Language
+alias Guessmoji.Media.Category
+alias Guessmoji.Media.Emoji
+alias Guessmoji.Media.Guess
