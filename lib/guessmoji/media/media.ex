d16e432dfe17eeb7e518232f5a55618c875ef04c
Elomar Nascimento dos Santos <elomarns@elomarns.com>
2018-04-01 17:49:30 -0300

    I love to delete useless code.

--- lib/guessmoji/media/media.ex
+++ lib/guessmoji/media/media.ex
@@ -4,10 +4,6 @@ defmodule Guessmoji.Media do
 
   alias Guessmoji.Media.Language
 
-  def list_languages do
-    Repo.all(Language)
-  end
-
   def get_language!(id), do: Repo.get!(Language, id)
 
   def get_language_by(fields), do: Repo.get_by(Language, fields)
@@ -18,25 +14,7 @@ defmodule Guessmoji.Media do
     |> Repo.insert()
   end
 
-  def update_language(%Language{} = language, attrs) do
-    language
-    |> Language.changeset(attrs)
-    |> Repo.update()
-  end
-
-  def delete_language(%Language{} = language) do
-    Repo.delete(language)
-  end
-
-  def change_language(%Language{} = language) do
-    Language.changeset(language, %{})
-  end
-
   alias Guessmoji.Media.Category
-
-  def list_categories do
-    Repo.all(Category)
-  end
 
   def get_category!(id), do: Repo.get!(Category, id)
 
@@ -48,25 +26,7 @@ defmodule Guessmoji.Media do
     |> Repo.insert()
   end
 
-  def update_category(%Category{} = category, attrs) do
-    category
-    |> Category.changeset(attrs)
-    |> Repo.update()
-  end
-
-  def delete_category(%Category{} = category) do
-    Repo.delete(category)
-  end
-
-  def change_category(%Category{} = category) do
-    Category.changeset(category, %{})
-  end
-
   alias Guessmoji.Media.Emoji
-
-  def list_emojis do
-    Repo.all(Emoji)
-  end
 
   def get_emoji!(id), do: Repo.get!(Emoji, id)
 
@@ -81,42 +41,16 @@ defmodule Guessmoji.Media do
     |> Repo.insert()
   end
 
-  def update_emoji(%Emoji{} = emoji, attrs) do
-    emoji
-    |> Emoji.changeset(attrs)
-    |> Repo.update()
-  end
-
-  def delete_emoji(%Emoji{} = emoji) do
-    Repo.delete(emoji)
-  end
-
   def change_emoji(%Emoji{} = emoji) do
     Emoji.changeset(emoji, %{})
   end
 
   alias Guessmoji.Media.Guess
 
-  def list_guesses do
-    Repo.all(Guess)
-  end
-
-  def get_guess!(id), do: Repo.get!(Guess, id)
-
   def create_guess(attrs \\ %{}) do
     %Guess{}
     |> Guess.changeset(attrs)
     |> Repo.insert()
-  end
-
-  def update_guess(%Guess{} = guess, attrs) do
-    guess
-    |> Guess.changeset(attrs)
-    |> Repo.update()
-  end
-
-  def delete_guess(%Guess{} = guess) do
-    Repo.delete(guess)
   end
 
   def change_guess(%Guess{} = guess) do
--- test/guessmoji/media/media_test.exs
+++ test/guessmoji/media/media_test.exs
@@ -5,11 +5,6 @@ defmodule Guessmoji.MediaTest do
   describe "languages" do
     alias Guessmoji.Media.Language
 
-    test "list_languages/0 returns all languages" do
-      language = language_fixture()
-      assert Media.list_languages() == [language]
-    end
-
     test "get_language!/1 returns the language with given id" do
       language = language_fixture()
       assert Media.get_language!(language.id) == language
@@ -39,42 +34,10 @@ defmodule Guessmoji.MediaTest do
 
       assert %{name: ["has already been taken"]} = errors_on(changeset)
     end
-
-    test "update_language/2 with valid data updates the language" do
-      language = language_fixture()
-      update_attrs = language_update_attrs()
-      assert {:ok, %Language{} = language} = Media.update_language(language, update_attrs)
-      assert language.name == update_attrs.name
-    end
-
-    test "update_language/2 with invalid data returns error changeset" do
-      language = language_fixture()
-
-      assert {:error, %Ecto.Changeset{}} =
-               Media.update_language(language, language_invalid_attrs())
-
-      assert language == Media.get_language!(language.id)
-    end
-
-    test "delete_language/1 deletes the language" do
-      language = language_fixture()
-      assert {:ok, %Language{}} = Media.delete_language(language)
-      assert_raise Ecto.NoResultsError, fn -> Media.get_language!(language.id) end
-    end
-
-    test "change_language/1 returns a language changeset" do
-      language = language_fixture()
-      assert %Ecto.Changeset{} = Media.change_language(language)
-    end
   end
 
   describe "categories" do
     alias Guessmoji.Media.Category
-
-    test "list_categories/0 returns all categories" do
-      category = category_fixture()
-      assert Media.list_categories() == [category]
-    end
 
     test "get_category!/1 returns the category with given id" do
       category = category_fixture()
@@ -105,42 +68,10 @@ defmodule Guessmoji.MediaTest do
 
       assert %{name: ["has already been taken"]} = errors_on(changeset)
     end
-
-    test "update_category/2 with valid data updates the category" do
-      category = category_fixture()
-      update_attrs = category_update_attrs()
-      assert {:ok, %Category{} = category} = Media.update_category(category, update_attrs)
-      assert category.name == update_attrs.name
-    end
-
-    test "update_category/2 with invalid data returns error changeset" do
-      category = category_fixture()
-
-      assert {:error, %Ecto.Changeset{}} =
-               Media.update_category(category, category_invalid_attrs())
-
-      assert category == Media.get_category!(category.id)
-    end
-
-    test "delete_category/1 deletes the category" do
-      category = category_fixture()
-      assert {:ok, %Category{}} = Media.delete_category(category)
-      assert_raise Ecto.NoResultsError, fn -> Media.get_category!(category.id) end
-    end
-
-    test "change_category/1 returns a category changeset" do
-      category = category_fixture()
-      assert %Ecto.Changeset{} = Media.change_category(category)
-    end
   end
 
   describe "emojis" do
     alias Guessmoji.Media.Emoji
-
-    test "list_emojis/0 returns all emojis" do
-      emoji = emoji_fixture()
-      assert Media.list_emojis() == [emoji]
-    end
 
     test "get_emoji!/1 returns the emoji with given id" do
       emoji = emoji_fixture()
@@ -169,29 +100,6 @@ defmodule Guessmoji.MediaTest do
       assert {:error, %Ecto.Changeset{}} = Media.create_emoji(emoji_invalid_attrs())
     end
 
-    test "update_emoji/2 with valid data updates the emoji" do
-      emoji = emoji_fixture()
-      update_attrs = emoji_update_attrs()
-      assert {:ok, %Emoji{} = emoji} = Media.update_emoji(emoji, update_attrs)
-      assert emoji.content == update_attrs.content
-      assert emoji.decoded_content == update_attrs.decoded_content
-      assert emoji.tip == update_attrs.tip
-      assert emoji.language_id == update_attrs.language_id
-      assert emoji.category_id == update_attrs.category_id
-    end
-
-    test "update_emoji/2 with invalid data returns error changeset" do
-      emoji = emoji_fixture()
-      assert {:error, %Ecto.Changeset{}} = Media.update_emoji(emoji, emoji_invalid_attrs())
-      assert emoji == Media.get_emoji!(emoji.id)
-    end
-
-    test "delete_emoji/1 deletes the emoji" do
-      emoji = emoji_fixture()
-      assert {:ok, %Emoji{}} = Media.delete_emoji(emoji)
-      assert_raise Ecto.NoResultsError, fn -> Media.get_emoji!(emoji.id) end
-    end
-
     test "change_emoji/1 returns a emoji changeset" do
       emoji = emoji_fixture()
       assert %Ecto.Changeset{} = Media.change_emoji(emoji)
@@ -200,16 +108,6 @@ defmodule Guessmoji.MediaTest do
 
   describe "guesses" do
     alias Guessmoji.Media.Guess
-
-    test "list_guesses/0 returns all guesses" do
-      guess = guess_fixture()
-      assert Media.list_guesses() == [guess]
-    end
-
-    test "get_guess!/1 returns the guess with given id" do
-      guess = guess_fixture()
-      assert Media.get_guess!(guess.id) == guess
-    end
 
     test "create_guess/1 with valid data creates a guess" do
       valid_attrs = guess_valid_attrs()
@@ -220,26 +118,6 @@ defmodule Guessmoji.MediaTest do
 
     test "create_guess/1 with invalid data returns error changeset" do
       assert {:error, %Ecto.Changeset{}} = Media.create_guess(guess_invalid_attrs())
-    end
-
-    test "update_guess/2 with valid data updates the guess" do
-      guess = guess_fixture()
-      update_attrs = guess_update_attrs()
-      assert {:ok, %Guess{} = guess} = Media.update_guess(guess, update_attrs)
-      assert guess.emoji_id == update_attrs.emoji_id
-      assert guess.content == update_attrs.content
-    end
-
-    test "update_guess/2 with invalid data returns error changeset" do
-      guess = guess_fixture()
-      assert {:error, %Ecto.Changeset{}} = Media.update_guess(guess, guess_invalid_attrs())
-      assert guess == Media.get_guess!(guess.id)
-    end
-
-    test "delete_guess/1 deletes the guess" do
-      guess = guess_fixture()
-      assert {:ok, %Guess{}} = Media.delete_guess(guess)
-      assert_raise Ecto.NoResultsError, fn -> Media.get_guess!(guess.id) end
     end
 
     test "change_guess/1 returns a guess changeset" do
