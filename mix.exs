243d267a4aea9f329a92cb78696bfa0deb31ebb8
Elomar Nascimento dos Santos <elomarns@elomarns.com>
2018-04-13 03:26:23 -0300

    And now edeliver!

--- mix.exs
+++ mix.exs
@@ -41,7 +41,8 @@ defmodule Guessmoji.Mixfile do
       {:phoenix_live_reload, "~> 1.0", only: :dev},
       {:gettext, "~> 0.11"},
       {:cowboy, "~> 1.0"},
-      {:distillery, "~> 1.5"}
+      {:distillery, "~> 1.5"},
+      {:edeliver, "~> 1.5"}
     ]
   end
 
